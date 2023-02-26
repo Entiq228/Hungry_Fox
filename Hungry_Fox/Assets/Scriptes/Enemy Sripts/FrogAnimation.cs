8b8d0f9cc640fa6bce5667a4ab10640707c371d1
Entiq228 <pavlodigalevic@gmail.com>
2023-02-26 13:35:45 +0200

    EnemyAnimation

---
+++ Hungry_Fox/Assets/Scriptes/Enemy Sripts/FrogAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogAnimation : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask ground;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(!Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground))
+        {
+            anim.SetBool("Jumping", true);
+        }
+        else if(Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground))
+        {
+            anim.SetBool("Jumping", false);
+        }
+    }
+}
---
+++ Hungry_Fox/Assets/Scriptes/Enemy Sripts/OpossumAnimation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpossumAnimation : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
